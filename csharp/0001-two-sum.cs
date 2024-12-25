7ddd8f6209479e6c023f9fdb716ccae5a662c4cc
Abhishek E H <collegewap@gmail.com>
2024-12-25 17:53:18 +0530

    fix c sharp solution

--- csharp/0001-two-sum.cs
+++ csharp/0001-two-sum.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
         Dictionary<int, int> seen = new Dictionary<int, int>();
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.Length; i++) {
             if (seen.ContainsKey(target - nums[i])) {
                 return new int[] {i, seen[target - nums[i]]};
             }
